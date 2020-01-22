fc2303cfceeaecd750b73d9e3ad284f4eebf4f13
JayChenFE <jaychenfe@foxmail.com>
2020-01-22 10:07:32 +0800

    实现WordCountCollector (#5)
    实现WordCountCollector (#5)

--- src/main/java/com/github/hcsp/WordCountCollector.java
+++ src/main/java/com/github/hcsp/WordCountCollector.java
@@ -1,3 +1,77 @@
 package com.github.hcsp;
 
-public class WordCountCollector {}
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class WordCountCollector implements Collector<String, Map<String, Integer>, Map<String, Integer>> {
+
+    /**
+     * A function that creates and returns a new mutable result container.
+     *
+     * @return a function which returns a new, mutable result container
+     */
+    @Override
+    public Supplier<Map<String, Integer>> supplier() {
+        return HashMap::new;
+    }
+
+    /**
+     * A function that folds a value into a mutable result container.
+     *
+     * @return a function which folds a value into a mutable result container
+     */
+    @Override
+    public BiConsumer<Map<String, Integer>, String> accumulator() {
+        return (map, str) -> map.merge(str, 1, Integer::sum);
+    }
+
+    /**
+     * A function that accepts two partial results and merges them.  The
+     * combiner function may fold state from one argument into the other and
+     * return that, or may return a new result container.
+     *
+     * @return a function which combines two partial results into a combined
+     * result
+     */
+    @Override
+    public BinaryOperator<Map<String, Integer>> combiner() {
+        return (map1, map2) -> {
+            map2.forEach((key, value) -> map1.merge(key, value, Integer::sum));
+            return map1;
+        };
+    }
+
+    /**
+     * Perform the final transformation from the intermediate accumulation type
+     * {@code A} to the final result type {@code R}.
+     *
+     * <p>If the characteristic {@code IDENTITY_FINISH} is
+     * set, this function may be presumed to be an identity transform with an
+     * unchecked cast from {@code A} to {@code R}.
+     *
+     * @return a function which transforms the intermediate result to the final
+     * result
+     */
+    @Override
+    public Function<Map<String, Integer>, Map<String, Integer>> finisher() {
+        return Function.identity();
+    }
+
+    /**
+     * Returns a {@code Set} of {@code Collector.Characteristics} indicating
+     * the characteristics of this Collector.  This set should be immutable.
+     *
+     * @return an immutable set of collector characteristics
+     */
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.emptySet();
+    }
+}
