11ec82ef37b3dbb85cc724bf0e8bfbb83337b886
hah1346798520 <51789422+hah1346798520@users.noreply.github.com>
2020-05-02 15:57:13 +0800

    编写一个自定义的Collector (#13)
    编写一个自定义的Collector (#13)

--- src/main/java/com/github/hcsp/WordCountCollector.java
+++ src/main/java/com/github/hcsp/WordCountCollector.java
@@ -1,3 +1,39 @@
 package com.github.hcsp;
 
-public class WordCountCollector {}
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class WordCountCollector implements Collector<String, Map<String, Integer>, Map<String, Integer>> {
+    @Override
+    public Supplier<Map<String, Integer>> supplier() {
+        return HashMap::new;
+    }
+
+    @Override
+    public BiConsumer<Map<String, Integer>, String> accumulator() {
+        return (map, s) -> map.put(s, map.getOrDefault(s, 0) + 1);
+    }
+
+    @Override
+    public BinaryOperator<Map<String, Integer>> combiner() {
+        return (map1, map2) -> {
+            map2.forEach((k, v) -> map1.merge(k, v, Integer::sum));
+            return map1;
+        };
+    }
+
+    @Override
+    public Function<Map<String, Integer>, Map<String, Integer>> finisher() {
+        return map -> map;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+    }
+
+}
