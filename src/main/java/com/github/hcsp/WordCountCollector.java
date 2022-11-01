b67afec6d6856f9f73e1ff1bdafd33ff35affaac
JiangTaoHou <925147954@qq.com>
2022-11-01 10:28:29 +0800

    resolve

--- src/main/java/com/github/hcsp/WordCountCollector.java
+++ src/main/java/com/github/hcsp/WordCountCollector.java
@@ -1,3 +1,39 @@
 package com.github.hcsp;
 
-public class WordCountCollector {}
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class WordCountCollector implements Collector<String, Map<String, Integer>, Map<String, Integer>> {
+    @Override
+    public Supplier<Map<String, Integer>> supplier() {
+        return HashMap::new;
+    }
+
+    @Override
+    public BiConsumer<Map<String, Integer>, String> accumulator() {
+        return (map, str) -> map.put(str, map.getOrDefault(str, 0) + 1);
+    }
+
+    @Override
+    public BinaryOperator<Map<String, Integer>> combiner() {
+        return null;
+    }
+
+
+    @Override
+    public Function<Map<String, Integer>, Map<String, Integer>> finisher() {
+        return map -> map;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.emptySet();
+    }
+}
